3a9fe5abc908f2e3e2e4b9958282277730eb8776
Steven <StevenRichardBuckle@gmail.com>
2019-11-07 14:47:00 -0500

    Selector Base Code/Model
    Added a Selector GameObject that has the begginings of selecting and spawning in objects.

---
+++ Assets/SRB_Stuff/Script/Selector.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Selector : MonoBehaviour
+{
+
+    GameObject Pointer;
+    public GameObject[] Objects;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Pointer = GameObject.Find("Pointer");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            GameObject g = Instantiate(Objects[0], Pointer.transform.position, Pointer.transform.rotation)
+                as GameObject;
+        }
+    }
+}
