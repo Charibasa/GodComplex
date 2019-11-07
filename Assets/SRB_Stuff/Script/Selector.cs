5ba2de79f7b79130b3027ce781ea1c65762d06c2
Steven <StevenRichardBuckle@gmail.com>
2019-11-07 18:34:06 -0500

    Update Selector.cs

--- Assets/SRB_Stuff/Script/Selector.cs
+++ Assets/SRB_Stuff/Script/Selector.cs
@@ -6,21 +6,25 @@ public class Selector : MonoBehaviour
 {
 
     GameObject Pointer;
+    GameObject SelectScreen;
     public GameObject[] Objects;
 
     // Start is called before the first frame update
     void Start()
     {
         Pointer = GameObject.Find("Pointer");
+        SelectScreen = GameObject.Find("SelectScreen");
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetButtonUp("Jump"))
         {
             GameObject g = Instantiate(Objects[0], Pointer.transform.position, Pointer.transform.rotation)
                 as GameObject;
+            //GameObject h = Instantiate(Objects[0], SelectScreen.transform.position, SelectScreen.transform.rotation)
+                //as GameObject;
         }
     }
 }
